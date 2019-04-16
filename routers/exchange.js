e2195743dfa08d4528198fbb3eb7cadd983a54c0
Jacek Majda <jacekmajd@gmail.com>
2019-04-16 21:09:04 +0200

    css added

--- routers/exchange.js
+++ routers/exchange.js
@@ -9,23 +9,22 @@ router.get("/", async (req, res) => {
         let currencyRates
         let ratesTable
         let ratesDate
-        const uri = 'http://api.nbp.pl/api/exchangerates/tables/a?format=json' //requesting whole data
+        const uri = 'http://api.nbp.pl/api/exchangerates/tables/a?format=json' //requesting whole data from api
         const getRates = {
             uri,
             json: true //parsing it to json if before parsing goes wrong
         }
 
         await rp(getRates).then((exchangeRates) => { //getting whole data using request-promises
-            currencyRates = exchangeRates.body[0].rates //saveing it to rates variable
-            ratesTable = exchangeRates.body[0].table //saveing it to rates variable
-            ratesDate = exchangeRates.body[0].effectiveDate //saveing it to rates variable
+            currencyRates = exchangeRates.body[0].rates //saveing rates to variable
+            ratesTable = exchangeRates.body[0].table //saveing exchange table to variable
+            ratesDate = exchangeRates.body[0].effectiveDate //saveing date to variable
         }).catch(err => {
             res.status(500).send({
                 error: 'something went wrong, try again later' //catching error
             })
         })
-        console.log(currencyRates)
-        res.render('index/index', {
+        res.render('index/index', { //rendering page and sending via handlebar data
             currencyRates,
             ratesDate,
             ratesTable
