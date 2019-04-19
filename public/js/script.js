922bcf97ba74e91d162dac478e1b3b8e6cea9bd4
Jacek Majda <jacekmajd@gmail.com>
2019-04-19 11:56:31 +0200

    tooltip added

--- public/js/script.js
+++ public/js/script.js
@@ -3,25 +3,6 @@ const currencyValue = [...document.querySelectorAll('.foreginQuantity')]
 const resultToExchange = document.querySelector('.resultToExchange')
 const plnToExchange = document.querySelector('.plnToExchange')
 const selectedCurrency = document.querySelector('.selectedCurrency')
-const linkedin = document.querySelector('.social1')
-const github = document.querySelector('.social2')
-const gmail = document.querySelector('.social3')
-const faccebook = document.querySelector('.social4')
-
-// links social icons to pages
-
-linkedin.addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/jacek-majda-aab81b17b/', '_blank');
-})
-github.addEventListener('click', () => {
-    window.open('https://github.com/Jackoski', '_blank');
-})
-gmail.addEventListener('click', () => {
-    window.open("mailto:mail@example.org", '_blank');
-})
-faccebook.addEventListener('click', () => {
-    window.open('https://www.facebook.com/jacek.majda', '_blank');
-})
 
 // currency exchange calculator
 
